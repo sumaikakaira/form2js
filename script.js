11c66ac73c77500913feec7c07e046b78050c3d4
sumaikakaira <122632122+sumaikakaira@users.noreply.github.com>
2023-04-11 23:42:54 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+// assignment no 4
+document.write('<h1> Registration Form</h1>');
+var firstName=prompt('Enter your Name?');
+var email=prompt('Enter your email?');
+var number=prompt('Enter your number?');
+var Age=prompt('Enter your Age?');
+var city=prompt('Enter your city?');
+var country=prompt('Enter your country?');
+var religion=prompt('Enter your Religion?');
+var passwrd=prompt('Enter your pssward?');
+var qualification=prompt('Enter your qualification?');
+var dof=prompt('Enter your date of birth?');
+document.write("Your Name:"+firstName);
+document.write('</br>');
+document.write('</br>');
+document.write("Enter your Email:"+email)
+document.write('</br>');
+document.write("</br>");
+document.write("Enter your number:"+number)
+document.write('</br>');
+document.write("</br>");
+document.write('Date of birth:'+dof)
+document.write('</br>');
+document.write("</br>");
+document.write('Age:'+Age)
+document.write('</br>');
+document.write("</br>");
+document.write('city:'+city)
+document.write('</br>');
+document.write("</br>");
+document.write('Country:'+country)
+document.write('</br>');
+document.write("</br>");
+document.write('Religion:'+religion)
+document.write('</br>');
+document.write("</br>");
+document.write("pssward:"+passwrd)
+document.write('</br>');
+document.write("</br>");
+// login detail
+var UserName=prompt('what is your Name');
+if(UserName==="Asadkakar"){
+    document.write('you are enrolled')
+}
+else{
+    document.write('sorry!you are not enrolled')
+};
